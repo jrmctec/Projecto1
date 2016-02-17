ee9f789e2f3a5e202b02a61b9612183f79b7faca
Jorge Roman <georgehawkblack@gmail.com>
2016-02-17 10:37:01 -0700

    +Determinar el tamaño de la ventana, +Liberar la consola al terminar ejecucion +Agregar visibilidad a la ventana +Agregar ventana principal

--- VentanaLogin.java
+++ VentanaLogin.java
@@ -3,6 +3,11 @@ import java.awt.*;
 
 public class VentanaLogin extends JFrame{
 	public static void main(String ar[]){
-
-	}//main
+	String title = "Acceso";
+	JFrame frame = new JFrame(title);
+	frame.pack();	
+	frame.setVisible(true);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	frame.setSize(300,300);	
+}//main
 }//class
